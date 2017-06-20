533be9102b600b0aac4b2cd9d392f78e31b4b234
sitr <sigbjorn@trageton.net>
2017-06-20 10:11:43 +0200

    Untrck config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-config.gpsEndPoint = 'http://localhost:2000';
-config.yrApiLocationUrl = 'https://yr.no/api/v0/locations/id/%s';
-
-//Max distance in kilometers between train and station. If greater, show Yr screen only
-config.stationMaxDistance = 5;
-
-module.exports = config;
